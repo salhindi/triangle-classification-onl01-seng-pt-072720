ef5539c7be96255433c43f9debc9e65bf57f709e
Sara A <calisaraalhindi@gmail.com>
2020-09-03 10:59:55 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,15 +1,18 @@
 class Triangle
   attr_accessor :side_a, :side_b, :side_c
   
-  def initialize(:side_a, :side_b, :side_c)
+  def initialize(side_a:, side_b:, side_c:)
+    @side_a= side_a
+    @side_b= side_b
+    @side_c= side_c
+  end
+    
       
   
   
   def kind(equilateral, isosceles, scalene)
-    if self != 
+end
       #isosceles 2 equal sides
       #equilateral 3 equal sides 
       #scalene no equal sides
-end
-    
-end
+
