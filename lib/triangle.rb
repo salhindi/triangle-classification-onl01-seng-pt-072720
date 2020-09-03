6a540468d1ab708989fd74eba8f438d2a7c3cb36
Sara A <calisaraalhindi@gmail.com>
2020-09-03 11:10:22 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,6 +11,7 @@ class Triangle
   if @side_a == @side_b || @side_a == @side_c || @side_c == @side_a
     :equilateral
   elsif @side_a == @side_b || @side_a == @side_c || @side_c == @side_a
+  :isosceles
   end
 end
       class TriangleError < StandardError
