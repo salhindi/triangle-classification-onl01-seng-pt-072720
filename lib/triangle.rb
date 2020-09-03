d3bee7441d7e2e4ed705fa8844badd0dafcfa664
Sara A <calisaraalhindi@gmail.com>
2020-09-03 11:02:25 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,7 +1,7 @@
 class Triangle
   attr_accessor :side_a, :side_b, :side_c
   
-  def initialize(side_a, side_b, side_c)
+  def initialize
     @side_a= side_a
     @side_b= side_b
     @side_c= side_c
