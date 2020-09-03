62e351957cffded5cf8b157af088110a4bc8523b
Sara A <calisaraalhindi@gmail.com>
2020-09-03 11:08:02 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,7 +8,7 @@ class Triangle
   end
     
   def kind()
-  if side_a != side_b || side_a != side_c || side_c != side_a
+  if @side_a != @side_b || @side_a != @side_c || @side_c != @side_a
     return TriangleError
   end
 end
