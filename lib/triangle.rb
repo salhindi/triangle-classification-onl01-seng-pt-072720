e59ea659f28a1d3111ec17c36b311e56e378fd84
Sara A <calisaraalhindi@gmail.com>
2020-09-03 11:33:35 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,7 +8,8 @@ class Triangle
   end
     
   def kind()
-    if 
+    if @side_a <= 0 || @side_b <= 0 || @side_c <= 0
+      raise TriangleError
     else @side_a == @side_b && @side_a == @side_c && @side_c == @side_a
       :equilateral
     elsif @side_a == @side_b || @side_a == @side_c || @side_c == @side_b
